121972d55b7c40d56f0b78f8d72f2c3b25973e6a
Maroš Varchola <marosvarchola@sunray.sk>
2026-03-03 01:01:35 +0100

    revert(notification): restore custom event evaluation across hooks

--- frappe/model/document.py
+++ frappe/model/document.py
@@ -1244,7 +1244,7 @@ class Document(BaseDocument):
 				_evaluate_alert(alert)
 			elif alert.event == "Method" and method == alert.method:
 				_evaluate_alert(alert)
-			elif alert.event == "Custom" and method == "on_change":
+			elif alert.event == "Custom":
 				_evaluate_alert(alert)
 
 	def _submit(self):
